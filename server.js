5830f36757a7b5da4bdee6e95ecdf1e8e2759b9d
M. Leary <learicist@gmail.com>
2019-04-05 13:37:06 -0400

    error removal, page load live

--- server.js
+++ server.js
@@ -3,17 +3,22 @@
 
 //check through and verify each
 // var connect      = require('connect');
+var path         = require('path');
 var express      = require('express');
 var app          = express();
 var port         = process.env.PORT || 3000;
 
 
 
+
 // require('./app/routes.js')(app);
 
+app.use(express.static(path.join(__dirname + '/views/'))); // relative path to serve static files
+
 app.get('/', function (req, res) {
-	res.send('Hello World');
+	res.sendFile('index.html', {root: './views'});
 });
+
 
 
 app.listen(3000, function(){
--- views/js/main.js
+++ views/js/main.js
@@ -1,13 +1,37 @@
 $(document).ready(function () {
 	console.log('Hello');
 	
-	$.ajax({
-	   url: "/hello_mike",
+	
+	$('#title').on({
+		
+		'mouseenter': function () { 
+			$(this).css('cursor', 'pointer');
+		},
+		
+		/*'click': function () {
+			
+			
+			
+		}*/
+		
+		
+	});
+	
+	
+	
+	
+	
+	
+	
+	
+	//make the call to the python script
+	/*$.ajax({
+	   url: "/hello_mike.py",
 	   success: function(response) {
 		 // here you do whatever you want with the response variable
 		 alert(response);
 		 
 	   }
-	});
+	});*/
 	
 });
